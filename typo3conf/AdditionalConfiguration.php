519785abf5cb20b047d20ff62bd94d333ac0a934
dmytro <dmytro@pixelant.se>
2016-04-20 16:06:46 +0200

    [CHORE] add new environments for TYPO3 context

--- typo3conf/AdditionalConfiguration.php
+++ typo3conf/AdditionalConfiguration.php
@@ -7,11 +7,11 @@ switch (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()) {
 		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = FALSE;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'VAGRANT :: TYPOkit';
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = 1;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = TRUE;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = 30711;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = 30709;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'Vagrant :: t3kit :: TYPO3';
 	break;
 	case 'Development':
 		// SetEnv TYPO3_CONTEXT Development
@@ -20,14 +20,62 @@ switch (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()) {
 		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = FALSE;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'DEV :: TYPOkit';
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'Development :: t3kit :: TYPO3';
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = 1;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = TRUE;
 	break;
 	case 'Testing':
 		// SetEnv TYPO3_CONTEXT Testing
 		// $GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'dbnamefor_test';
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'TEST :: TYPOkit';
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'Testing :: t3kit :: TYPO3';
+	break;
+	case 'Development/Demo':
+		// SetEnv TYPO3_CONTEXT Development/Demo
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = TRUE;
+		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = FALSE;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = 1;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = TRUE;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = 30711;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = 30709;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'Demo :: t3kit :: TYPO3';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'demo_t3kit';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 'demo_t3kit';
+	break;
+	case 'Development/DemoTestserver':
+		// SetEnv TYPO3_CONTEXT Development/DemoTestserver
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = TRUE;
+		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = FALSE;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = 1;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = TRUE;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = 30711;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = 30709;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'DemoTestserver :: t3kit :: TYPO3';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'demo_t3kit_tests';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 'demo_t3kit_tests';
+	break;
+	case 'Development/TestingTestserver':
+		// SetEnv TYPO3_CONTEXT Development/TestingTestserver
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = TRUE;
+		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = FALSE;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = 1;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = TRUE;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = 30711;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = 30709;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'TestingTestserver :: t3kit :: TYPO3';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'test_t3kit_tests';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 'test_t3kit_tests';
+	break;
+	case 'Development/t3kitTestserver':
+		// SetEnv TYPO3_CONTEXT Development/t3kitTestserver
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'Testserver :: t3kit :: TYPO3';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 't3kit_tests';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 't3kit_tests';
 	break;
 }
 
