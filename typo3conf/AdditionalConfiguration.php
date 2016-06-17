7993da31e8a76b7b2e905e1c3805f48c36744aae
Mats Svensson <mats@pixelant.se>
2016-06-17 13:25:54 +0200

    [FEATURE] added production context in AdditionalConfiguration

--- typo3conf/AdditionalConfiguration.php
+++ typo3conf/AdditionalConfiguration.php
@@ -77,6 +77,21 @@ switch (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()) {
 		$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 't3kit_tests';
 		$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 't3kit_tests';
 	break;
+    case 'Production':
+        // SetEnv TYPO3_CONTEXT Production
+        $GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = 0;
+        $GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = 0;
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '';
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 0;
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['enableDeprecationLog'] = '';
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = 0;
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = 0;
+        $GLOBALS['TYPO3_CONF_VARS']['FE']['disableNoCacheParameter'] = 1;
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = 30466; // Default = 30466
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = 20480; // Default = 20480
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['syslogErrorReporting'] = 4437; // Default = 30711
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['belogErrorReporting'] = 4437; // Default = 30711
+    break;
 }
 
 ?>
