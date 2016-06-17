a05fc03dc68a921e30ed9b9a412fb4426a51ff7c
dmh <dmytro.hrynevych@gmail.com>
2016-06-17 11:12:26 +0300

    add comments to custom.js file

---
+++ fileadmin/custom/js/custom.js
@@ -0,0 +1,3 @@
+// this file is disabled by default.
+// If you need to use custom.js in your project you need to enable it in TS.
+// themes_t3kit/Configuration/TypoScript/Library/page.includeJS.setupts
