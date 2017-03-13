73a15a6726c81aabe704f96c3052aa5182debbdf
dmh <dmytro.hrynevych@gmail.com>
2017-03-13 16:19:26 +0200

    [TASK] docker, move TYPO3 temp out of shared folders to improve speed

--- docker_conf/run.sh
+++ docker_conf/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-chown -R www-data:www-data /var/www/html
+# stop apache
+apache2ctl stop
 
+# Moving TYPO3 temp out of shared folders to improve speed
+mkdir /typo3temp
+chown -R www-data /typo3temp
+rm -rf /var/www/html/typo3temp
+ln -s /typo3temp typo3temp
+
+# start apache
 exec apache2ctl -D FOREGROUND
