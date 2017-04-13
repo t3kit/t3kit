1ab791368fba20a2493d252b3892238dbde8bf67
Alex Bachynskyi <alex@pixelant.se>
2017-04-13 12:16:06 +0300

    [TASK] added mailhog to docker

--- typo3conf/LocalConfiguration.php
+++ typo3conf/LocalConfiguration.php
@@ -61,11 +61,11 @@ return [
         'processor_path_lzw' => '/usr/bin/',
     ],
     'MAIL' => [
-        'transport' => 'sendmail',
+        'transport' => 'smtp',
         'transport_sendmail_command' => '/usr/sbin/sendmail -t -i ',
         'transport_smtp_encrypt' => '',
         'transport_smtp_password' => '',
-        'transport_smtp_server' => '',
+        'transport_smtp_server' => 'mailhog:1025',
         'transport_smtp_username' => '',
     ],
     'SYS' => [
