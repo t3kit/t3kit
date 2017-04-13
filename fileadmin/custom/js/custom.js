a5b48569be7530a17ee61ced7f186b87f2c4b995
dmh <dmytro.hrynevych@gmail.com>
2017-04-13 18:52:33 +0300

    [TASK] remove custom.js, relocate custom.css/less

--- fileadmin/custom/js/custom.js
+++
@@ -1,3 +0,0 @@
-// this file is disabled by default.
-// If you need to use custom.js in your project you need to enable it in TS.
-// themes_t3kit/Configuration/TypoScript/Library/page.includeJS.setupts
