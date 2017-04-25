5c27b57dacab6f1b871e3e4912eae6bdf7a77dee
dmh <dmytro.hrynevych@gmail.com>
2017-04-25 17:38:30 +0300

    [TASK] set userID for docker mac configuration

--- docker_conf/macos/run.sh
+++ docker_conf/macos/run.sh
@@ -2,7 +2,23 @@
 
 LOGFILE=/tmp/docker-compose.txt
 
-chown -R www-data /var/www/
+chown -R www-data /var/www/html/
+
+# Make www-data user and group use id:s from hosts user to make shared folder writable.
+#
+# fetch current userid and groupid for user www-data
+WWW_DATA_USERID=$(id -u www-data)
+WWW_DATA_GROUPID=$(id -g www-data)
+# if current userid doesn't equal the one from yaml, force id of user to the one from yaml
+if [ "$WWW_DATA_USERID" -ne "${USERID}" ];then
+    echo "run.sh : usermod -u ${USERID} www-data" >> $LOGFILE
+    usermod -u ${USERID} www-data
+fi
+# if current groupid doesn't equal the one from yaml, force id of group to the one from yaml
+if [ "$WWW_DATA_GROUPID" -ne "${GROUPID}" ];then
+    echo "run.sh : groupmod -g ${GROUPID} www-data" >> $LOGFILE
+    groupmod -g ${GROUPID} www-data
+fi
 
 # set solrdata group to 8983 to make folder writable for solr container
 chown :8983 /var/www/html/solrdata/ -R
