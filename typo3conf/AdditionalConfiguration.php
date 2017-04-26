9b7c09d91a282af5e7eca8f7605d8afc9dd08599
Mats Svensson <mats@pixelant.se>
2017-04-26 08:44:14 +0200

    [FIX] update setttings in AddtionalConfiguration for TYPO3 version 8

--- typo3conf/AdditionalConfiguration.php
+++ typo3conf/AdditionalConfiguration.php
@@ -1,37 +1,16 @@
 <?php
 
 switch (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()) {
-	case 'Development/Vagrant':
-		// SetEnv TYPO3_CONTEXT Development/Vagrant
-		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = TRUE;
-		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = FALSE;
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = 1;
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = TRUE;
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = 30711;
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = 30709;
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'Vagrant :: t3kit :: TYPO3';
-		// Set systemLogLevel to 0 to enable systemLog
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLogLevel'] = 0;
-		// Will then appear in http://localhost:28778/ (logio)
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLog'] = 'error_log';
-		// Will then appear in http://localhost:1080/ (mailcatcher) and http://localhost:28778/ (logio)
-		// Page will load noticably slow when systemLog to "mail" is enabled and a lot of errors occurs.
-		// $GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLog'] = 'mail,dummy@t3kit.com;error_log';
-
-	break;
 	case 'Development/Docker':
 		// SetEnv TYPO3_CONTEXT Development
-		$GLOBALS['TYPO3_CONF_VARS']['DB']['host'] = 'db';
-		$GLOBALS['TYPO3_CONF_VARS']['DB']['port'] = '3307';
-		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = TRUE;
-		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = FALSE;
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] = 'db';
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = true;
+		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = false;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'Docker :: t3kit :: TYPO3';
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = 1;
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = TRUE;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = true;
 		$GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport'] = 'smtp';
 		$GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_smtp_server'] = 't3kit_mailhog:1025';
 		// Page will load noticably slow when systemLog to "mail" is enabled and a lot of errors occurs.
@@ -39,67 +18,67 @@ switch (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()) {
 	break;
 	case 'Development':
 		// SetEnv TYPO3_CONTEXT Development
-		// $GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'dbnamefor_dev';
-		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = TRUE;
-		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = FALSE;
+		// $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = 'dbnamefor_dev';
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = true;
+		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = false;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'Development :: t3kit :: TYPO3';
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = 1;
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = TRUE;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = true;
 	break;
 	case 'Testing':
 		// SetEnv TYPO3_CONTEXT Testing
-		// $GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'dbnamefor_test';
+		// $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = 'dbnamefor_test';
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'Testing :: t3kit :: TYPO3';
 	break;
 	case 'Development/Demo':
 		// SetEnv TYPO3_CONTEXT Development/Demo
-		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = TRUE;
-		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = FALSE;
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = true;
+		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = false;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = 1;
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = TRUE;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = true;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = 30711;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = 30709;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'Demo :: t3kit :: TYPO3';
-		$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'demo_t3kit';
-		$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 'demo_t3kit';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = 'demo_t3kit';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = 'demo_t3kit';
 	break;
 	case 'Development/DemoTestserver':
 		// SetEnv TYPO3_CONTEXT Development/DemoTestserver
-		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = TRUE;
-		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = FALSE;
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = true;
+		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = false;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = 1;
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = TRUE;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = true;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = 30711;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = 30709;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'DemoTestserver :: t3kit :: TYPO3';
-		$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'demo_t3kit_tests';
-		$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 'demo_t3kit_tests';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = 'demo_t3kit_tests';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = 'demo_t3kit_tests';
 	break;
 	case 'Development/TestingTestserver':
 		// SetEnv TYPO3_CONTEXT Development/TestingTestserver
-		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = TRUE;
-		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = FALSE;
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = true;
+		$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = false;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = 1;
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = TRUE;
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = true;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = 30711;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = 30709;
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'TestingTestserver :: t3kit :: TYPO3';
-		$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'test_t3kit_tests';
-		$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 'test_t3kit_tests';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = 'test_t3kit_tests';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = 'test_t3kit_tests';
 	break;
 	case 'Development/t3kitTestserver':
 		// SetEnv TYPO3_CONTEXT Development/t3kitTestserver
 		$GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'Testserver :: t3kit :: TYPO3';
-		$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 't3kit_tests';
-		$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 't3kit_tests';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = 't3kit_tests';
+		$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = 't3kit_tests';
 	break;
     case 'Production':
         // SetEnv TYPO3_CONTEXT Production
@@ -120,10 +99,8 @@ switch (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()) {
         // Disable debug mode for dyncss
         if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['dyncss'])) {
             $dynCssConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['dyncss']);
-            $dynCssConfiguration['enableDebugMode'] = FALSE;
+            $dynCssConfiguration['enableDebugMode'] = false;
             $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['dyncss'] = $dynCssConfiguration;
         }
     break;
 }
-
-?>
