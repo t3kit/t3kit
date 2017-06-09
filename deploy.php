2457632f0aeec62dc848c33b950c8291b10ed994
Mattias Nilsson <mattias@pixelant.se>
2017-06-09 11:12:37 +0200

    [TASK] Add code documentation to deployment script

--- deploy.php
+++ deploy.php
@@ -12,21 +12,25 @@ host('')
     ->stage('production')
     ->set('deploy_path', '');
 
+// Installation vendors through composer
 desc('Installing vendors');
 task('deploy:vendors', function () {
     run('cd {{deploy_path}} && {{env_vars}} {{bin/composer}} {{composer_options}}');
 });
 
+// TYPO3 Database migration
 desc('TYPO3 Database migration(s)');
 task('database:migration', function () {
     run('cd {{deploy_path}} && vendor/bin/typo3cms database:updateschema');
 });
 
+// Flush TYPO3 cache
 desc('TYPO3 Flush cache');
 task('flush:cache', function () {
     run('cd {{deploy_path}} && vendor/bin/typo3cms cache:flush');
 });
 
+// Bundled deployment chain
 task('deploy', [
     'deploy:prepare',
     'deploy:lock',
@@ -37,7 +41,8 @@ task('deploy', [
     'cleanup',
 ])->desc('Deploy your project');
 
+// If a deployment is successful
 after('deploy', 'success');
 
-// If deploy fails automatically unlock.
+// If deploy fails automatically unlock
 after('deploy:failed', 'deploy:unlock');
