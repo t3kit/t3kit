2b3291a490ca59c1364ac3aef3214335c6beab6a
dmh <dmytro.hrynevych@gmail.com>
2019-04-19 17:16:38 +0300

    add TCA config

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,3 @@
+<?php
+defined('TYPO3_MODE') || die();
+
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,2 +1,7 @@
 <?php
 defined('TYPO3_MODE') || die();
+
+// Add PageTS config
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TsConfig/Page/page.tsconfig">'
+);
