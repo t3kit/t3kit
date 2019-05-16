feceef3cd85aed5c2c4ecb202b7988ed942a1baa
dmh <dmytro.hrynevych@gmail.com>
2019-05-16 14:39:45 +0300

    [TASK] add t3kit system info to System toolbar item

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,75 @@
 <?php
+
 defined('TYPO3_MODE') || die();
 
-// Add PageTS config
+/*
+ * ###########################
+ * Add t3kit PageTS config
+ * ===========================
+ */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
     '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TsConfig/Page/page.tsconfig">'
 );
+
+/*
+ * ################################################
+ * Add t3kit system info to System toolbar item
+ * ===============================================
+ */
+$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+$signalSlotDispatcher->connect(
+        \TYPO3\CMS\Backend\Backend\ToolbarItems\SystemInformationToolbarItem::class,
+        'getSystemInformation',
+        \T3k\t3kit\Backend\ToolbarItems\SystemInformationToolbar\T3kitToolbarItems::class,
+        'addSeparator'
+);
+$signalSlotDispatcher->connect(
+        \TYPO3\CMS\Backend\Backend\ToolbarItems\SystemInformationToolbarItem::class,
+        'getSystemInformation',
+        \T3k\t3kit\Backend\ToolbarItems\SystemInformationToolbar\T3kitToolbarItems::class,
+        'getT3kitVersion'
+);
+$signalSlotDispatcher->connect(
+        \TYPO3\CMS\Backend\Backend\ToolbarItems\SystemInformationToolbarItem::class,
+        'getSystemInformation',
+        \T3k\t3kit\Backend\ToolbarItems\SystemInformationToolbar\T3kitToolbarItems::class,
+        'getT3kitMode'
+);
+
+/*
+ * ###########################
+ * Register Icons
+ * ===========================
+ */
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+// System information icons
+$iconRegistry->registerIcon(
+    'systeminformation-t3kit',
+    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+    ['source' => 'EXT:t3kit/Resources/Public/Icons/bmp.svg']
+);
+// Content Elements icons
+$contentElementIconFilePath = 'EXT:t3kit/Resources/Public/Icons/ContentElements/';
+$contentElementIcons = [
+    'ce-slider',
+    'ce-bigIconTextButton',
+    'ce-iconTextButton',
+    'ce-imageTextLink',
+    'ce-logoCarousel',
+    'ce-quote',
+    'ce-fullWidthImage',
+    'ce-responsiveVideo',
+    'ce-socialIcons',
+    'ce-copyrightText',
+    'ce-contacts',
+    'ce-button',
+    'ce-heroImage',
+    'ce-contactsCard'
+];
+foreach ($contentElementIcons as $icon) {
+    $iconRegistry->registerIcon(
+        $icon,
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => $contentElementIconFilePrefix . $icon . '.svg']
+    );
+}
--- ext_tables.php
+++ ext_tables.php
@@ -1,2 +1,15 @@
 <?php
+
 defined('TYPO3_MODE') || die();
+
+/*
+ * ###########################
+ * Add  Context Sensitive Help (CSH)
+ * ===========================
+ */
+// TODO:
+// \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+//     'tt_content',
+//     'EXT:t3kit/Resources/Private/Language/locallang_csh_tt_content.xlf'
+// );
+
