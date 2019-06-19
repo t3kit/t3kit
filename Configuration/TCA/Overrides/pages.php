5c7a3d466a6d1e59ba138633cc96a088c0446da4
dmh <dmytro.hrynevych@gmail.com>
2019-06-19 18:25:01 +0300

    fix path for PageTS includes

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,3 +1,16 @@
 <?php
 defined('TYPO3_MODE') || die();
 
+call_user_func(function()
+{
+    /*
+    * ###########################
+    * PageTS for t3kit
+    * ===========================
+    */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+        't3kit',
+        'Configuration/TsConfig/Page/PageTS.tsconfig',
+        't3kit'
+    );
+});
