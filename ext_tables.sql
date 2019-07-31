863dd8c5fe73535dcc7cc979383d8adb0a48e941
dmh <dmytro.hrynevych@gmail.com>
2019-07-31 16:17:23 +0300

    [TASK] add SiteHeader element

--- ext_tables.sql
+++ ext_tables.sql
@@ -17,4 +17,31 @@ CREATE TABLE tt_content (
 	background int(11) unsigned DEFAULT '0' NOT NULL,
 	background_parallax tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	full_width_background tinyint(3) unsigned DEFAULT '0' NOT NULL,
+
+	skipLinks_mainNavigation varchar(255) DEFAULT '' NOT NULL,
+	skipLinks_mainContent varchar(255) DEFAULT '' NOT NULL,
+
+	headerTop int(11) unsigned DEFAULT '0' NOT NULL,
+	headerTopContacts int(11) unsigned DEFAULT '0' NOT NULL,
+	headerTopLangMenu int(11) unsigned DEFAULT '0' NOT NULL,
+	headerTopSearch int(11) unsigned DEFAULT '0' NOT NULL,
+	headerTopNavigation int(11) unsigned DEFAULT '0' NOT NULL,
+
+	email_address varchar(255) DEFAULT '' NOT NULL,
+	email_title varchar(255) DEFAULT '' NOT NULL,
+	email_icon int(11) unsigned DEFAULT '0' NOT NULL,
+	email_show_title int(11) unsigned DEFAULT '0' NOT NULL,
+
+	phone_link varchar(255) DEFAULT '' NOT NULL,
+	phone_text varchar(60) DEFAULT '' NOT NULL,
+	phone_title varchar(255) DEFAULT '' NOT NULL,
+	phone_icon int(11) unsigned DEFAULT '0' NOT NULL,
+	phone_show_title int(11) unsigned DEFAULT '0' NOT NULL,
+
+
+	headerMiddle int(11) unsigned DEFAULT '0' NOT NULL,
+
+	mainNavigation int(11) unsigned DEFAULT '0' NOT NULL,
+
+
 );
