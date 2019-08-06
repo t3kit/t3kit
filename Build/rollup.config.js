5b521ab722809f525baa0d4de1fcf514a9546740
Mikkel Hastrup <mikkel@pixelant.se>
2019-08-06 16:19:19 +0200

    [TASK] added uglifyjs to npm. removed minified suffix in filename in rollup. added js-minify-command.

--- Build/rollup.config.js
+++ Build/rollup.config.js
@@ -4,7 +4,7 @@ import resolve from 'rollup-plugin-node-resolve';
 export default {
     input: 'dev/js/bootstrap.js',
     output: {
-        file: '../Resources/Public/JavaScript/bundle.min.js',
+        file: '../Resources/Public/JavaScript/bundle.js',
         format: 'iife',
         name: 'bundle'
     },
