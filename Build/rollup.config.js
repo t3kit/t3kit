d5cd428aa694677a36696b9b3173ddfeaf5338bf
Mosa Al-Husseini <mosaalhusseini@gmail.com>
2019-08-06 14:11:35 +0200

    [WIP] add old felayout dev folder, removed js/css/less from old felayout, added rollup.config file, bootstrap 4.3.1

---
+++ Build/rollup.config.js
@@ -0,0 +1,25 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+    input: 'dev/js/bootstrap.js',
+    output: {
+        file: '../Resources/Public/JavaScript/bundle.min.js',
+        format: 'iife',
+        name: 'bundle'
+    },
+    plugins: [
+        babel({
+            exclude: 'node_modules/**',
+            externalHelpersWhitelist: [ // Include only required helpers
+              'defineProperties',
+              'createClass',
+              'inheritsLoose',
+              'defineProperty',
+              'objectSpread'
+            ]
+        }),
+        resolve()
+    ]
+}
+
