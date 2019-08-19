62ef509c317faf1e9368d48b2eb43b85746a5055
Mosa Al-Husseini <mosaalhusseini@gmail.com>
2019-08-19 13:50:05 +0200

    [TASK] remove unused js files, compile js/css

--- Build/dev/js/bootstrap.js
+++ Build/dev/js/bootstrap.js
@@ -1,5 +1,6 @@
 // BOOTSTRAP
 //==========================================================
+import '../../node_modules/bootstrap/js/dist/util.js';
 import '../../node_modules/bootstrap/js/dist/alert.js';
 import '../../node_modules/bootstrap/js/dist/button.js';
 import '../../node_modules/bootstrap/js/dist/carousel.js';
