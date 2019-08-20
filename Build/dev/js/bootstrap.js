94ef2e6a75743a3e2563530e690aa44298f6cb02
Mosa Al-Husseini <mosaalhusseini@gmail.com>
2019-08-20 14:16:53 +0200

    [TASK] compile js files

--- Build/dev/js/bootstrap.js
+++ Build/dev/js/bootstrap.js
@@ -1,15 +1,15 @@
 // BOOTSTRAP
 //==========================================================
-import '../../node_modules/bootstrap/js/dist/util.js';
-import '../../node_modules/bootstrap/js/dist/alert.js';
-import '../../node_modules/bootstrap/js/dist/button.js';
-import '../../node_modules/bootstrap/js/dist/carousel.js';
-import '../../node_modules/bootstrap/js/dist/collapse.js';
+import '../../node_modules/bootstrap/js/src/util.js';
+import '../../node_modules/bootstrap/js/src/alert.js';
+import '../../node_modules/bootstrap/js/src/button.js';
+import '../../node_modules/bootstrap/js/src/carousel.js';
+import '../../node_modules/bootstrap/js/src/collapse.js';
 // import '../../node_modules/bootstrap/js/dropdown.js';
 // import '../../node_modules/bootstrap/js/modal.js';
 // import '../../node_modules/bootstrap/js/tooltip.js';
 // import '../../node_modules/bootstrap/js/popover.js';
 // import '../../node_modules/bootstrap/js/scrollspy.js';
-import '../../node_modules/bootstrap/js/dist/tab.js';
+import '../../node_modules/bootstrap/js/src/tab.js';
 // import '../../node_modules/bootstrap/js/affix.js';
 //==========================================================
