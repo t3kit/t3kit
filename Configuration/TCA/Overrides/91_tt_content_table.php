a80c77ddb589b1dc4c8bfdf4947583a26b37f0ae
Mats Svensson <mats@pixelant.se>
2020-01-03 15:06:44 +0100

    [CLEANUP] restructure code to make lines shorter

--- Configuration/TCA/Overrides/91_tt_content_table.php
+++ Configuration/TCA/Overrides/91_tt_content_table.php
@@ -7,13 +7,34 @@ $GLOBALS['TCA']['tt_content']['columns']['table_class']['config'] = [
     'type' => 'select',
     'renderType' => 'selectCheckBox',
     'items' => [
-        ['LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-dark', 'table-dark'],
-        ['LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-striped', 'table-striped'],
-        ['LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-bordered', 'table-bordered'],
-        ['LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-borderless', 'table-borderless'],
-        ['LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-hover', 'table-hover'],
-        ['LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-sm', 'table-sm'],
-        ['LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-responsive', 'table-responsive'],
+        [
+            'LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-dark',
+            'table-dark'
+        ],
+        [
+            'LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-striped',
+            'table-striped'
+        ],
+        [
+            'LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-bordered',
+            'table-bordered'
+        ],
+        [
+            'LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-borderless',
+            'table-borderless'
+        ],
+        [
+            'LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-hover',
+            'table-hover'
+        ],
+        [
+            'LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-sm',
+            'table-sm'
+        ],
+        [
+            'LLL:EXT:t3kit/Resources/Private/Language/locallang_BE_CE.xlf:table_class.table-responsive',
+            'table-responsive'
+        ],
     ],
     'default' => ''
 ];
