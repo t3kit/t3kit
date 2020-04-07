7d627f25bc4bf997b9c2c5468bd08f13f56197db
Mosa Al-Husseini <mosaalhusseini@gmail.com>
2020-04-07 07:38:43 +0200

    [TASK] changes to heroImage element

--- theme/src/js/main/contentElements/heroImage.js
+++ theme/src/js/main/contentElements/heroImage.js
@@ -10,12 +10,7 @@
     $frame.each(function () {
       var self = $(this)
       var $p = self.find('.hero-image__caption-p')
-      if ($p.length) {
-        $p.dotdotdot({
-          watch: 'window',
-          height: 55
-        })
-      }
+      
       if (!self.parents('.swiper-wrapper').length) {
         self.addClass('_animated')
       }
