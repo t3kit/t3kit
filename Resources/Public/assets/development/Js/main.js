d5ad47f4b841f1b5af02eaaf6f9a0c5a5604dd03
Mosa <mosaalhusseini@gmail.com>
2020-04-07 09:46:14 +0200

    [TASK] add changes to heroImage element (#233)

--- Resources/Public/assets/development/Js/main.js
+++ Resources/Public/assets/development/Js/main.js
@@ -196,5 +196,23 @@
     });
   });
 
+  /* global jQuery */
+  (function ($) {
+
+    // document load event
+    $(document).ready(function () {
+      var $frame = $('.js__hero-image');
+      // var $slider = $('.slider-container')
+      $frame.each(function () {
+        var self = $(this);
+        var $p = self.find('.hero-image__caption-p');
+        
+        if (!self.parents('.swiper-wrapper').length) {
+          self.addClass('_animated');
+        }
+      });
+    });
+  })(jQuery);
+
 }());
 //# sourceMappingURL=main.js.map
--- theme/src/js/main.js
+++ theme/src/js/main.js
@@ -1,3 +1,4 @@
 import './main/contentElements/content1'
 import './main/contentElements/content2'
 import './main/header/header'
+import './main/contentElements/heroImage'
