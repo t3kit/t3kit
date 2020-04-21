cf386fd3d1020842f481fb452cfdf22406d82e76
dmh <dmytro.hrynevych@gmail.com>
2020-04-21 14:17:54 +0300

    [TASK] update to TYPO3 v10.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,8 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.2.0-10.4.99',
+            'typo3' => '10.4.0-10.4.99',
+            'php' => '7.2.0-7.4.99'
         ],
         'conflicts' => [
             'css_styled_content' => '*',
