f22ed2373478cd0ed5f44bd1a7ac7b700c32481a
dmh <dmytro.hrynevych@gmail.com>
2020-07-24 14:41:50 +0300

    [TASK] add t3kit default UserTS options

--- ext_localconf.php
+++ ext_localconf.php
@@ -54,9 +54,12 @@ if (!(bool) $t3kitextConf['disableTCEMAIN']) {
     );
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    "@import 'EXT:t3kit/Configuration/TSconfig/User/Options.tsconfig'"
-);
+// USERTS
+if (!(bool) $t3kitextConf['disableUserTSoptions']) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
+        "@import 'EXT:t3kit/Configuration/TSconfig/User/Options.tsconfig'"
+    );
+}
 
 // Define TypoScript as content rendering template
 $GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'][] = 't3kit/Configuration/TypoScript/';
