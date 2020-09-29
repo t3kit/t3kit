310e62842dace224f7da3c1cd6d286d62569378e
dmh <dmytro.hrynevych@gmail.com>
2020-09-29 17:54:56 +0300

    [BUGFIX] fix import TypoScript for extensions

--- Configuration/TCA/Overrides/50_sys_template.php
+++ Configuration/TCA/Overrides/50_sys_template.php
@@ -12,40 +12,26 @@ defined('TYPO3_MODE') || die();
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     't3kit',
     'Configuration/TypoScript',
-    'All t3kit Configuration'
+    '--- All t3kit Configuration'
 );
 
 // Include Content Elements Configuration
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     't3kit',
     'Configuration/TypoScript/ContentElement',
-    'Content Elements Configuration'
+    '--- Content Elements Configuration'
 );
 
 // Include Page Configuration
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     't3kit',
     'Configuration/TypoScript/Page',
-    'Page Configuration'
+    '--- Page Configuration'
 );
 
 // Include General Config
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     't3kit',
     'Configuration/TypoScript/Config',
-    'General Config'
-);
-
-// Include t3kit indexed_search config
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    't3kit',
-    'Configuration/TypoScript/Plugin/IndexedSearch',
-    'Indexed Search Configuration'
-);
-
-// Include t3kit SEO config
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    't3kit',
-    'Configuration/TypoScript/Plugin/Seo',
-    'SEO Configuration'
+    '--- General Config'
 );
