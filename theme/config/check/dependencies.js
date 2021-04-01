12cae412fbd48ab3b58f76e8f2eafb1572421c11
dmh <dmh@users.noreply.github.com>
2021-04-01 12:44:23 +0300

    [TASK] update dep-s using npm7, restrict npm version

--- theme/config/check/dependencies.js
+++ theme/config/check/dependencies.js
@@ -1,11 +1,12 @@
 const fs = require('fs')
 const nodeEngine = require('../../../package').engines.node
+const npmVer = require('../../../package').engines.npm
 
 function checkNodeModules (localConf) {
   if (!fs.existsSync(`${localConf.rootPath}/node_modules`)) {
     console.log('Error: No node modules installed in this extension')
     console.log('Please install all required dependencies by running <npm install>')
-    console.log(`You should use Node.js ${nodeEngine}\n`)
+    console.log(`You should use Node.js ${nodeEngine} and NPM ${npmVer}\n`)
     process.exit(1)
   }
 }
